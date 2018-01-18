406e17127f37f533393537b2eb0d07a08416786f
Aram Zucker-Scharff <azuckers@gmu.edu>
2018-01-18 14:15:44 -0500

    Enforce overflow with mutation observe
    for when other options aren't available.

--- nop.js
+++ nop.js
@@ -1,15 +1,17 @@
-var MAXIMUM_INTERVAL =  (1000*60)*1;
-function activateBodyNOPs(){
-	if ( Object.getOwnPropertyDescriptor(document.documentElement.style, 'overflow').configurable ){
+var MAXIMUM_INTERVAL = (1000 * 60) * 2;
+
+function activateBodyNOPs() {
+	console.log('Activate NOPs');
+	if (Object.getOwnPropertyDescriptor(document.documentElement.style, 'overflow').configurable) {
 		//document.documentElement.style = document.documentElement.style || {}; document.documentElement.style.overflow = "auto";
 		Object.defineProperty(
 			document.documentElement.style,
 			"overflow", {
-				set: function(val){
+				set: function (val) {
 					console.log("NOPed on style overflow", val);
 					return true;
 				},
-				get: function(){
+				get: function () {
 					return 'auto';
 				},
 				//writable: false,
@@ -25,7 +27,9 @@ function activateBodyNOPs(){
 		// We will instead brute force a lock on this style.
 		console.log("nop", "Can't freeze documentElement style", "");
 		window.setInterval(
-			function(){ document.documentElement.style.overflow = 'auto'; },
+			function () {
+				document.documentElement.style.overflow = 'auto';
+			},
 			60,
 			true
 		);
@@ -52,15 +56,16 @@ function activateBodyNOPs(){
 	Object.freeze(document.getElementsByTagName('body')[0].style);
 	**/
 	//document.body.style = document.body.style || {};
-	if ( document.body.hasOwnProperty('style') && Object.getOwnPropertyDescriptor(document.body.style, 'overflow').configurable ){
+	if (document.body.hasOwnProperty('style') && Object.getOwnPropertyDescriptor(document.body.style, 'overflow').configurable) {
+		console.log('Overwrite body style overflow');
 		Object.defineProperty(
 			document.body.style,
 			"overflow", {
-				set: function(val){
+				set: function (val) {
 					console.log("nop", "NOPed on style overflow", val);
 					return true;
 				},
-				get: function(){
+				get: function () {
 					return 'auto';
 				},
 				//writable: false,
@@ -69,28 +74,43 @@ function activateBodyNOPs(){
 				//value: 'auto'
 			}
 		);
+	} else if ('MutationObserver' in window) {
+		console.log('Enforce overflow with Mutation Observer');
+		var observer = new MutationObserver(function (mutations) {
+			mutations.forEach(function (mutationRecord) {
+				console.log('style changed!', mutationRecord);
+				if (document.body.style.overflow !== 'auto') {
+					document.body.style.overflow = 'auto';
+				}
+			});
+		});
+
+		observer.observe(document.body, {
+			attributes: true,
+			attributeFilter: ['style']
+		});
+
+	} else {
+		// Safari won't let us freeze this property properly.
+		// We will instead brute force a lock on this style.
+		console.log("nop", "Can't freeze document body style", "");
+		window.setInterval(
+			function () {
+				document.body.style.overflow = 'auto';
+			},
+			60,
+			true
+		);
 	}
 }
 
 var setIntervalOrig = window.setInterval;
-window.setInterval = function(callback, intervalTime, override){
+window.setInterval = function (callback, intervalTime, override) {
 	var intervalID = setIntervalOrig(callback, intervalTime);
-	if (typeof override === 'undefined' || !override){
-		window.setTimeout(function(){
+	if (typeof override === 'undefined' || !override) {
+		window.setTimeout(function () {
 			window.clearInterval(intervalID);
-			console.log('setInterval maximum of '+MAXIMUM_INTERVAL+' reached on interval ID:', intervalID);
-		}, MAXIMUM_INTERVAL);
-	}
-	return intervalID;
-}
-
-var setTimeoutOrig = window.setTimeout;
-window.setInterval = function(callback, intervalTime, override){
-	var intervalID = setIntervalOrig(callback, intervalTime);
-	if (typeof override === 'undefined' || !override){
-		window.setTimeout(function(){
-			window.clearInterval(intervalID);
-			console.log('setInterval maximum of '+MAXIMUM_INTERVAL+' reached on interval ID:', intervalID);
+			console.log('setInterval maximum of ' + MAXIMUM_INTERVAL + ' reached on interval ID:', intervalID);
 		}, MAXIMUM_INTERVAL);
 	}
 	return intervalID;
